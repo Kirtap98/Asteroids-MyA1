d4db75fd9263b71c4004c7442b46ad918a590b94
alejandro-Gonza <alejandro-alejandro.gonzacordova@davinci.edu.ar>
2020-04-27 20:16:53 -0300

    Movimiento y Rotacion del Personaje
    primeros pasos

---
+++ Asteroids MyA1/Assets/Scripts/Player/Player_Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    public float speedPlayer;
+    public float rotatePlayer;
+
+    Rigidbody2D _rg2d;
+
+    void Start()
+    {
+        _rg2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        RotatePlayer();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+       _rg2d.AddForce(transform.up * Input.GetAxis("Vertical") * speedPlayer, ForceMode2D.Impulse);
+    }
+
+    void RotatePlayer()
+    {
+        _rg2d.rotation += Input.GetAxis("Horizontal") * rotatePlayer;
+    }
+}
